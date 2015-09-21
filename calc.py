462d405b799d28cd36785b0b9676f62d428ccd49
marta castejon cachero <mcastejo@zeta40.aulas.gsyc.es>
2015-09-21 11:00:47 +0200

    Añadido suma y mult

--- calc.py
+++ calc.py
@@ -3,8 +3,19 @@
 
 # Vamos a programar una calculadora
 
+import sys
+
 def main():
     pass
-
+def suma(sum1, sum2):
+    return sum1 + sum2
+def multiplicación(ope1, ope2):
+    return ope1*ope2
 if __name__ == "__main__":
-    main()
+    try:
+        operando1 = int(sys.argv[2]) # ya que nos muestra una lista con cadenas de        caracteres y la tenemos que pasar a int
+        operando2 = int(sys.argv[3])
+    execpt ValueError:
+        sys.exit('¡Deben ser números enteros!')
+    print(suma(operando1,operando2))
+    print(multiplicacion(operando1,operando2))
