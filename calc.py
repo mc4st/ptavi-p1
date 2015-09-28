4cbbef2521e5de71646e38670b3944033dc04c5c
marta castejon cachero <mcastejo@zeta40.aulas.gsyc.es>
2015-09-28 09:48:39 +0200

    terminado

--- calc.py
+++ calc.py
@@ -9,13 +9,24 @@ def main():
     pass
 def suma(sum1, sum2):
     return sum1 + sum2
-def multiplicación(ope1, ope2):
+def resta(sum1, sum2):
+    return sum1 - sum2
+def multiplicacion(ope1, ope2):
     return ope1*ope2
+def division(ope1, ope2):
+    return ope1/ope2
 if __name__ == "__main__":
     try:
-        operando1 = int(sys.argv[2]) # ya que nos muestra una lista con cadenas de        caracteres y la tenemos que pasar a int
+        operando1 = int(sys.argv[1]) # ya que nos muestra una lista con cadenas de        caracteres y la tenemos que pasar a int
         operando2 = int(sys.argv[3])
-    execpt ValueError:
+        operacion = sys.argv[2]
+    except ValueError:
         sys.exit('¡Deben ser números enteros!')
-    print(suma(operando1,operando2))
-    print(multiplicacion(operando1,operando2))
+    if operacion == 'suma':
+        print(suma(operando1,operando2))
+    elif operacion == 'resta':
+        print(resta(operando1,operando2))
+    elif operacion == 'multiplicacion':
+        print(multiplicacion(operando1,operando2))
+    elif operacion == 'division':
+        print(division(operando1,operando2))
